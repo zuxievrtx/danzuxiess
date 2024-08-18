f8cd6d2b6abb02d104267f33978d317bd433690a
zuxievrtx <kulitayam571@protonmail.com>
2024-08-18 22:59:45 +0700

    danzuxie2

--- src/components/index/Footer.tsx
+++ src/components/index/Footer.tsx
@@ -1,14 +1,14 @@
 import Divider from "@/components/Divider";
 
 export default function Footer() {
-    return (
-        <>
-            <footer className="max-w-4xl w-full flex flex-col mx-auto pt-4 pb-2">
-                <Divider />
-                <h2 className="font-leaguespartan text-center font-semibold text-base text-text pt-2">
-                    Aiden.gg - Made with NextJS, TailwindCSS, and ❤ by me
-                </h2>
-            </footer>
-        </>
-    );
+  return (
+    <>
+      <footer className="max-w-4xl w-full flex flex-col mx-auto pt-4 pb-2">
+        <Divider />
+        <h2 className="font-leaguespartan text-center font-semibold text-base text-text pt-2">
+          Danzuxie.pp.ua - Made with NextJS, TailwindCSS, and ❤ by me
+        </h2>
+      </footer>
+    </>
+  );
 }
